6992dfb42ef5eed14f3d121b4aa6729d0ac50b42
00Govind00 <govindharikumar1999@gmail.com>
2026-05-21 09:21:55 +0530

    day3

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { About } from './pages/about/about';
+import { Contact } from './pages/contact/contact';
+import { Home } from './pages/home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: Home},
+    {
+        path: "about", component: About
+    },
+    {path:'contact', component:Contact}
+];
